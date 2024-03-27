f2392366b98b5c72f4fa3e4100cc62069947a790
gapirajat <rajat.shinde201@pccoepune.org>
2024-03-27 06:43:48 +0000

    update

--- JS/index.js
+++ JS/index.js
@@ -192,7 +192,7 @@ $('video').on('timeupdate',function(){
     
     var timeLeft = this.duration - this.currentTime;
     console.log(this.currentTime);
-      if (timeLeft <= 30 && !video.hasClass('fading')) {
+      if (timeLeft <= 20 && !video.hasClass('fading')) {
         console.log(timeLeft);
         video.addClass('fading');
         video.animate({opacity: 0}, 1); // Fade over 2 seconds
